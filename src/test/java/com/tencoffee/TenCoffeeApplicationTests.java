6f6bb9a1433605a7b540470ad9746419df47513e
LuigiCapoia <luigicapoia@gmail.com>
2024-07-24 10:53:04 -0300

    att esqueleto do projeto

---
+++ src/test/java/com/tencoffee/TenCoffeeApplicationTests.java
@@ -0,0 +1,11 @@
+package com.tencoffe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenCoffeApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
